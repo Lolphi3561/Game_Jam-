afc99b0ba09c4a716ff1bc1287c0bc51e1877297
Talha Zengin <168358523+Lolphi3561@users.noreply.github.com>
2026-02-02 11:27:41 +0100

    Physiks

--- Game Jam/Assets/CharacterScript.cs
+++ Game Jam/Assets/CharacterScript.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CharacterScript : MonoBehaviour
 {
+    public Rigidbody2D myRigidBody;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidBody.linearVelocity = Vector2.up * 10;
+        }
     }
 }

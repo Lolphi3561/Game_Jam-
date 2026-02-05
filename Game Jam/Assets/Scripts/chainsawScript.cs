b9005c449bd9e24ff8ba77c63c0dc42c876cad9b
Talha Zengin <168358523+Lolphi3561@users.noreply.github.com>
2026-02-05 13:22:50 +0100

    some sprites added

--- Game Jam/Assets/Scripts/chainsawScript.cs
+++ Game Jam/Assets/Scripts/chainsawScript.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class chainsawScript : MonoBehaviour
@@ -7,6 +8,8 @@ public class chainsawScript : MonoBehaviour
     private Vector3 targetPoint;
 
     public float chainsawSpeed = 0.2f;
+
+    public float chainsawRotationSpeed = -5f;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -19,16 +22,20 @@ public class chainsawScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        transform.Rotate(new Vector3(0, 0, -chainsawRotationSpeed));
+
         transform.position = Vector3.MoveTowards(transform.position, targetPoint, chainsawSpeed*Time.deltaTime);
         if(transform.position == targetPoint)
         {
             if (targetPoint == startPoint)
             {
                 targetPoint = endPoint;
+                chainsawRotationSpeed *= -1;
             }
             else
             {
                 targetPoint = startPoint;
+                chainsawRotationSpeed *= -1;
             }
         }
     }
